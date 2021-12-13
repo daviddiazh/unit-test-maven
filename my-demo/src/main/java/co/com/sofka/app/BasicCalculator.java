14b0244d1475cf71c91484d97f31db9e7368e5b8
David Diaz <80727430+daviddiazh@users.noreply.github.com>
2021-12-12 22:35:51 -0500

    Suma, resta, mult y divison listas

---
+++ my-demo/src/main/java/co/com/sofka/app/BasicCalculator.java
@@ -0,0 +1,32 @@
+package co.com.sofka.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long subtract(Long number1, Long number2) {
+        logger.info( "Subtracting {} - {}", number1, number2 );
+        return number1 - number2;
+    }
+    public Long multiply(Long number1, Long number2) {
+        logger.info( "Multiplying {} * {}", number1, number2 );
+        return number1 * number2;
+    }
+
+    public Long divide(Long number1, Long number2) {
+        logger.info( "Dividing {} / {}", number1, number2 );
+        if (number2 == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
+        return number1 / number2;
+    }
+
+}
